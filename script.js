b24c88add6e573e0914ff064f0a50679d6315161
Senthamizh1106 <rsthamizh2006@gmail.com>
2026-06-20 12:11:55 +0530

    version2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let applications = [];
+let applications =
+JSON.parse(localStorage.getItem("applications"))
+|| [];
+
+displayApplications();
 
 function addApplication(){
 
@@ -12,11 +16,14 @@ function addApplication(){
     document.getElementById("status").value;
 
     if(company === "" || role === ""){
+
         alert("Please fill all fields");
+
         return;
     }
 
     const application = {
+
         company,
         role,
         status
@@ -24,13 +31,23 @@ function addApplication(){
 
     applications.push(application);
 
+    saveData();
+
     displayApplications();
 
     document.getElementById("company").value = "";
     document.getElementById("role").value = "";
 }
 
-function displayApplications(){
+function saveData(){
+
+    localStorage.setItem(
+        "applications",
+        JSON.stringify(applications)
+    );
+}
+
+function displayApplications(data = applications){
 
     const list =
     document.getElementById("applicationList");
@@ -41,13 +58,14 @@ function displayApplications(){
     let interview = 0;
     let selected = 0;
 
-    applications.forEach((app,index)=>{
+    data.forEach((app,index)=>{
 
         if(app.status === "Applied") applied++;
         if(app.status === "Interview") interview++;
         if(app.status === "Selected") selected++;
 
         list.innerHTML += `
+
         <div class="application-card">
 
             <h3>${app.company}</h3>
@@ -55,18 +73,29 @@ function displayApplications(){
             <p>${app.role}</p>
 
             <span class="status">
-                ${app.status}
+            ${app.status}
             </span>
 
-            <br>
+            <br><br>
+
+            <button
+            class="edit-btn"
+            onclick="changeStatus(${index})">
+
+            Change Status
+
+            </button>
 
             <button
             class="delete-btn"
             onclick="deleteApplication(${index})">
-                Delete
+
+            Delete
+
             </button>
 
         </div>
+
         `;
     });
 
@@ -87,5 +116,75 @@ function deleteApplication(index){
 
     applications.splice(index,1);
 
+    saveData();
+
     displayApplications();
+}
+
+function changeStatus(index){
+
+    let current =
+    applications[index].status;
+
+    if(current === "Applied"){
+
+        applications[index].status =
+        "Interview";
+    }
+
+    else if(current === "Interview"){
+
+        applications[index].status =
+        "Selected";
+    }
+
+    else{
+
+        applications[index].status =
+        "Applied";
+    }
+
+    saveData();
+
+    displayApplications();
+}
+
+function searchApplications(){
+
+    const value =
+    document.getElementById("searchInput")
+    .value
+    .toLowerCase();
+
+    const filtered =
+    applications.filter(app =>
+
+        app.company
+        .toLowerCase()
+        .includes(value)
+    );
+
+    displayApplications(filtered);
+}
+
+function filterApplications(){
+
+    const filter =
+    document.getElementById("filterStatus")
+    .value;
+
+    if(filter === "All"){
+
+        displayApplications();
+
+        return;
+    }
+
+    const filtered =
+    applications.filter(app =>
+
+        app.status === filter
+    );
+
+    displayApplications(filtered);
 }
